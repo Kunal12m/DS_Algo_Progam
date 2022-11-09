4a3d053afd8c15f302b478c65cdcaf0ac85178a2
Kunal Mukherjee <Kunal_Mukherjee@epam.com>
2022-11-09 10:40:11 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+/**
+ * Created by Kunal Mukherjee on ${DATE}.
+ * Kunal_Mukherjee@epam.com
+ * EPAM System
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

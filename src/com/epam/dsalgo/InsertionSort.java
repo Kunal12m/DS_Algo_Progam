ba2a30f2729fd9e1bbffde22aab9c5c62a2df330
Kunal Mukherjee <Kunal_Mukherjee@epam.com>
2022-11-15 15:10:36 +0530

    Implement InsertionSort Algorithm with test cases

---
+++ src/com/epam/dsalgo/InsertionSort.java
@@ -0,0 +1,47 @@
+package com.epam.dsalgo;
+
+import com.epam.exception.NoArrayFoundException;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by Kunal Mukherjee on 15-11-2022.
+ * Kunal_Mukherjee@epam.com
+ * EPAM System
+ */
+public class InsertionSort {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = random.nextInt(1000);
+
+        InsertionSort is = new InsertionSort();
+        System.out.println(Arrays.toString(arr));
+        is.insertionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public void insertionSort(int[] arr) {
+        if (arr == null) throw new NoArrayFoundException("No Array Found");
+        if (arr.length == 0) return;
+        insertionSort(arr, 0, arr.length - 1);
+    }
+
+    private void insertionSort(int[] arr, int i, int size) {
+        int value = arr[i];
+        int j = i;
+        while (j > 0 && arr[j - 1] > value) {
+            arr[j] = arr[j - 1];
+            j--;
+        }
+
+        arr[j] = value;
+
+        if (i + 1 <= size) {
+            insertionSort(arr, i + 1, size);
+        }
+    }
+}
--- test/com/epam/dsalgo/InsertionSortTest.java
+++ test/com/epam/dsalgo/InsertionSortTest.java
@@ -16,49 +16,47 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * Kunal_Mukherjee@epam.com
  * EPAM System
  */
-public class QuickSortTest {
-    private static QuickSort quickSort;
+public class InsertionSortTest {
+    private static InsertionSort insertionSort;
     private static Random random;
 
     @BeforeAll
     public static void init() {
-        quickSort = new QuickSort();
+        insertionSort = new InsertionSort();
         random = new Random();
     }
 
     @Test
-    @DisplayName("quickSort Method Sort Small Input Array")
-    public void quickSort_method_sort_small_input_array() {
+    @DisplayName("insertionSort Method Sort Small Input Array")
+    public void insertionSort_method_sort_small_input_array() {
         int[] inputArr = new int[10];
         for (int i = 0; i < inputArr.length; i++)
             inputArr[i] = random.nextInt(1000);
         int[] sortArr = Arrays.stream(inputArr).sorted().toArray();
-        quickSort.quickSort(inputArr);
+        insertionSort.insertionSort(inputArr);
         assertArrayEquals(sortArr, inputArr);
     }
 
     @Test
-    @DisplayName("quickSort Method Sort Large Input Array")
-    public void quickSort_method_sort_large_input_array() {
+    @DisplayName("insertionSort Method Sort Large Input Array")
+    public void insertionSort_method_sort_large_input_array() {
         int[] inputArr = new int[100000];
         for (int i = 0; i < inputArr.length; i++)
             inputArr[i] = random.nextInt(10000);
-        int[] sortArr = Arrays.stream(inputArr).sorted().toArray();
-        quickSort.quickSort(inputArr);
-        assertArrayEquals(sortArr, inputArr);
+        assertThrows(StackOverflowError.class, () -> insertionSort.insertionSort(inputArr));
     }
 
     @Test
-    @DisplayName("quickSort Method With Empty Input Array")
-    public void quickSort_method_with_empty_input_array() {
+    @DisplayName("insertionSort Method With Empty Input Array")
+    public void insertionSort_method_with_empty_input_array() {
         int[] inputArr = new int[0];
-        quickSort.quickSort(inputArr);
+        insertionSort.insertionSort(inputArr);
         assertArrayEquals(new int[0], inputArr);
     }
 
     @Test
-    @DisplayName("quickSort Method With Null Input Array")
-    public void quickSort_method_with_Null_input_array() {
-        assertThrows(NoArrayFoundException.class, () -> quickSort.quickSort(null));
+    @DisplayName("insertionSort Method With Null Input Array")
+    public void insertionSort_method_with_Null_input_array() {
+        assertThrows(NoArrayFoundException.class, () -> insertionSort.insertionSort(null));
     }
 }

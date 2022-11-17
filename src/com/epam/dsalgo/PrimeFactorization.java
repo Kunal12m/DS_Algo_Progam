619acd369194558d4c5409f22ec6a79f4ea671a4
Kunal Mukherjee <Kunal_Mukherjee@epam.com>
2022-11-17 12:24:21 +0530

    Implementation PrimeFactorization

---
+++ src/com/epam/dsalgo/PrimeFactorization.java
@@ -0,0 +1,38 @@
+package com.epam.dsalgo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Kunal Mukherjee on 17-11-2022.
+ * Kunal_Mukherjee@epam.com
+ * EPAM System
+ */
+public class PrimeFactorization {
+
+    public static void main(String[] args) {
+        int[] primeFactor = new PrimeFactorization().primeFactorization(1207650011);
+        System.out.println(Arrays.toString(primeFactor));
+    }
+
+    public int[] primeFactorization(int x) {
+        List<Integer> primeFactor = new ArrayList<>();
+        for (int i = 2; i < Math.sqrt(x); i++) {
+            while ((x % i) == 0) {
+                primeFactor.add(i);
+                x /= i;
+            }
+        }
+        int[] ints;
+        if (x > 2) {
+            ints = new int[primeFactor.size() + 1];
+            ints[primeFactor.size()] = x;
+        } else ints = new int[primeFactor.size()];
+        int k = 0;
+        for (Integer integer : primeFactor) {
+            ints[k++] = integer;
+        }
+        return ints;
+    }
+}

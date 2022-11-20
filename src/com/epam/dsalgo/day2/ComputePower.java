0dcf681fe105a08e879b76b1243647ad576c5fb8
Kunal Mukherjee <Kunal_Mukherjee@epam.com>
2022-11-20 22:24:41 +0530

    Refactor Power Function

--- src/com/epam/dsalgo/day2/ComputePower.java
+++ src/com/epam/dsalgo/day2/ComputePower.java
@@ -11,23 +11,28 @@ public class ComputePower {
     public static void main(String[] args) {
         ComputePower cp = new ComputePower();
         Random random = new Random();
-        int a = random.nextInt(1000);
+        double a = random.nextDouble() * random.nextInt(1000);
         int n = random.nextInt(10);
-        System.out.printf("Compute %d power of %d\n", a, n);
+        System.out.printf("Compute %f power of %d\n", a, n);
         System.out.println(cp.power(a, n));
-        System.out.println(cp.newPower(a, n));
     }
 
-    public double power(int a, int n) {
-        if (n <= 0) return 1;
-        return a * power(a, n - 1);
+    public double power(double a, int n) {
+        if (n < 0) {
+            n = Math.abs(n);
+            a = 1 / a;
+        }
+        return pow(a, n);
     }
 
-    public double newPower(int a, int n) {
-        if (n <= 0) return 1;
-        if ((n & 1) == 0)
-            return newPower(a, n / 2) * newPower(a, n / 2);
-        else
-            return a * newPower(a, (n - 1) / 2) * newPower(a, (n - 1) / 2);
+    private double pow(double a, int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n % 2 == 0) {
+            return pow(a * a, n / 2);
+        } else {
+            return a * pow(a * a, n / 2);
+        }
     }
+
 }

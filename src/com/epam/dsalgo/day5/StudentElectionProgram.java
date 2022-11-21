1b3875b1f4f09067b5185aff61f4db29c3446177
Kunal Mukherjee <Kunal_Mukherjee@epam.com>
2022-11-21 10:02:29 +0530

    Implemented StudentElectionProgram

---
+++ src/com/epam/dsalgo/day5/StudentElectionProgram.java
@@ -0,0 +1,42 @@
+package com.epam.dsalgo.day5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Kunal Mukherjee on 21-11-2022.
+ * Kunal_Mukherjee@epam.com
+ * EPAM System
+ */
+public class StudentElectionProgram {
+
+    public static void main(String[] args) {
+        StudentElectionProgram electionProgram = new StudentElectionProgram();
+        int n = 6;
+        int songLength = 4;
+        System.out.println(electionProgram.whoIsElected(n, songLength));
+    }
+
+    public int whoIsElected(int numberOfStudents, int songLength) {
+        if (songLength == 1) return numberOfStudents;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= numberOfStudents; i++)
+            list.add(i);
+        songLength -= 1;
+        int i = 0;
+        while (list.size() > 1) {
+            if (list.size() > i + songLength) {
+                i += songLength;
+                list.remove(i);
+            } else {
+                i = songLength - (list.size() - i);
+                while (i > list.size()) {
+                    i -= list.size();
+                }
+                list.remove(i);
+            }
+            if (i == list.size()) i = 0;
+        }
+        return list.get(0);
+    }
+}
